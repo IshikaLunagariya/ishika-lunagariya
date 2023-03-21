36a1c81ff8d3ac08690f75350ff24648926ea09f
IshikaLunagariya <109205709+IshikaLunagariya@users.noreply.github.com>
2023-03-21 15:36:04 +0530

    main.dart file change

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:clock_simple/clock_simple.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  await SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  );
+  runApp(const ClockSimple());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const ClockSimple();
-  }
-}
-
